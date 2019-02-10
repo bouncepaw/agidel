dd8c592ab81e3742d458d2034ddce4118971573e
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-10 21:31:41 +0500

    Make agidel.syntrans rely on Chicken egg dir
    It turned out that maintaining AGIDEL_DIR/syntrans is troublesome

--- syntrans.scm
+++ syntrans.scm
@@ -5,11 +5,6 @@
          (chicken base)
          (prefix (agidel core) agidel/))
 
- ;; Return list of files with syntranses. `lst` is list of syntranses as
- ;; symbols. The order of files is the same as order in `lst`.
- (define (files lst)
-   (agidel/extension-files lst "syntrans" "es"))
-
  ;; Append '/main to each element of `lst`.
  (define (suffix-/main lst)
    (map (lambda (st) (symbol-append st '/main)) lst))
@@ -19,7 +14,6 @@
  ;;
  ;; `syntranses` is list of syntranses to use.
  (define (compose-λ syntranses)
-   (map load (files syntranses)) ; Load all syntrans files
    (eval (cons 'import (agidel/importify syntranses 'syntrans))) ; Import them
    (eval (list 'lambda '(source) ; And create function from them
                (foldl (lambda (acc next) (list next acc))
