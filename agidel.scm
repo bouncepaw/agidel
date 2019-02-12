96667eaa5b20339820f8c7988b99528ac441b24d
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-12 19:35:15 +0500

    Make agidel save list of plugins to use in AGIDEL_TMP_FILES
    Reason for choosing env var over passing an arg is explained in code itself.

--- agidel.scm
+++ agidel.scm
@@ -4,6 +4,7 @@ This is the main file in the whole Agidel ecosystem.
 (import (chicken process-context)
         (chicken io)
         (chicken file)
+        (chicken string)
         (srfi 69)
         (srfi 13)
         (srfi 1)
@@ -22,14 +23,19 @@ This is the main file in the whole Agidel ecosystem.
 ;; Main
 (let* ((args           (args/traverse (command-line-arguments)))
        (files          (hash-table-ref args 'files))
-       (plugin-paths   (plugin/files (hash-table-ref args 'plugins)))
-       (syntrans-λ     (syntrans/compose-λ (hash-table-ref args 'syntranses)))
-       (parsed-files   (-> (lambda (f)
-                             (-> f
-                                 open-input-file
-                                 (as-> x (read-string #f x))
-                                 syntrans-λ))
-                           (map files)
-                           (string-join "\n" 'suffix))))
-  (format #t "~A" parsed-files)
+       (syntrans-λ     (syntrans/compose-λ (hash-table-ref args 'syntranses))))
+  ;; I put this thing into env variable, because I am not aware of global
+  ;; variables in Chicken Scheme. I need a global variable to pass list of
+  ;; plugins to `prepare` syntrans. I can't pass it directly to the syntrans
+  ;; function, because syntranses are designed to work with strings only.
+  (set-environment-variable! "AGIDEL_TMP_PLUGINS"
+                             (->string (map string->symbol
+                                            (hash-table-ref args 'plugins))))
+  (format #t "~A" (-> (lambda (f)
+                        (-> f
+                            open-input-file
+                            (as-> x (read-string #f x))
+                            syntrans-λ))
+                      (map files)
+                      (string-join "\n" 'suffix)))
   )
