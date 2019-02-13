a65bb472de0d148ad22d69966a7214cfd85793f2
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-13 22:09:40 +0500

    Make `compose-λ` generate binary syntranses

--- syntrans.scm
+++ syntrans.scm
@@ -9,13 +9,14 @@
  (define (suffix-/main lst)
    (map (lambda (st) (symbol-append st '/main)) lst))
 
+
  ;; Return unary function which accepts a string and returns a string after
  ;; syntax transformation.
  ;;
  ;; `syntranses` is list of syntranses to use.
  (define (compose-λ syntranses)
    (eval (cons 'import (agidel/importify syntranses 'syntrans))) ; Import them
-   (eval (list 'lambda '(source) ; And create function from them
-               (foldl (lambda (acc next) (list next acc))
+   (eval (list 'lambda '(source plugins) ; And create function from them
+               (foldl (lambda (acc next) (list next acc 'plugins))
                       'source
                       (suffix-/main syntranses))))))
