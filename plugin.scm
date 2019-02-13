6c872639ca2904a8d5344d86216adefcd0831bce
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-13 22:13:03 +0500

    Remove plugin helpers

--- plugin.scm
+++ plugin.scm
@@ -15,20 +15,6 @@
  ;; Append '/_agidel-arities to each element of `lst`.
  (define (suffix-/_agidel-arities lst)
    (map (lambda (p) (symbol-append p '/_agidel-arities)) lst))
-
- ;; I put this thing into env variable, because I am not aware of global
- ;; variables in Chicken Scheme. I need a global variable to pass list of
- ;; plugins to `prepare` syntrans. I can't pass it directly to the syntrans
- ;; function, because syntranses are designed to work with strings only.
- (define (save-needed-plugins plugins)
-   (set-environment-variable! "AGIDEL_TMP_PLUGINS" (->string plugins)))
-
- ;; Get list of all needed plugins from env var AGIDEL_TMP_PLUGINS
- (define (needed-plugins)
-   (car (agidel/parse-string (get-environment-variable "AGIDEL_TMP_PLUGINS"))))
-
- (define (free-needed-plugins)
-   (set-environment-variable! "AGIDEL_TMP_PLUGINS" ""))
  
  ;; Return hash-table, where keys are function names and values are
  ;; lists like that: (q e q q . e). You know that well amigo.
