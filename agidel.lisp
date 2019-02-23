76016fd9606167c18b245ba6b9493a5642b167a0
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-23 18:59:39 +0500

    Use new version of Agidel/sh in agidel.lisp

--- agidel.lisp
+++ agidel.lisp
@@ -1,19 +1,18 @@
 (shebang!)
-(set 'import_statement
+(set import_statement
      "(import (prefix (only scheme define string-append display)
                       AGIDEL/)
               (only scheme quote)")
 (for-each-cli-arg
- 'plugin
- (set 'import_statement '+ "(agidel-plugin $plugin)"))
-(set 'import_statement '+ ")")
-['csi '$csi_args
-      '-batch '-quiet '-eval
-      "(begin
+ plugin
+ (set import_statement + "(agidel-plugin $plugin)"))
+(set import_statement + ")")
+[csi -batch -quiet -eval
+     "(begin
         (module agidel_temp (main)
                 $import_statement
                 (AGIDEL/define (main)
                    (AGIDEL/display (AGIDEL/string-append $(cat /dev/stdin)))))
         (import agidel_temp)
         (main))"]
-['echo]
+[echo]
